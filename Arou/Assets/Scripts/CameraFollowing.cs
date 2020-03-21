d166c51e8c8a535f0aae346f35c91d4a83d27cef
Wojtek <wojciech.brzuszkiewicz@hotmail.com>
2020-03-21 21:38:44 +0100

    Added camera following player object

---
+++ Arou/Assets/Scripts/CameraFollowing.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollowing : MonoBehaviour
+{
+    [SerializeField]
+    private Transform _playerObj;
+    [SerializeField]
+    private float _dotProduct = 0.0f;
+    private Vector3 _directionToPlayer = Vector3.zero;
+    private Vector3 _cameraPosOnSamePlane = Vector3.zero;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        _cameraPosOnSamePlane = new Vector3(transform.position.x, transform.position.y);
+        _directionToPlayer = (_playerObj.position - _cameraPosOnSamePlane).normalized;
+
+        if((_dotProduct = Vector3.Dot(transform.up, _directionToPlayer)) > 0)
+        {
+            var newCameraPos = new Vector3(transform.position.x, _playerObj.position.y, transform.position.z);
+            transform.position = Vector3.Lerp(transform.position, newCameraPos, 0.05f);
+        }
+
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.blue;
+        Gizmos.DrawLine(_cameraPosOnSamePlane, _cameraPosOnSamePlane + _directionToPlayer);
+    }
+}
